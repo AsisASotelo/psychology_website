7781ddde0b18866e1264abe578633ddff77082c9
AsisASotelo <asis.a.sotelo@gmail.com>
2020-07-02 22:20:24 -0700

    Initial Add Files

---
+++ markv0.1/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HeaderComponent from './components/HeaderComponent';
+
+function App() {
+  return (
+   <HeaderComponent/>
+  );
+}
+
+export default App;
---
+++ markv0.1/src/components/HeaderComponent.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+function HeaderComponent(props){
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+    <NavbarBrand href="/"><img width="100" src="ucsblogo.png"/></NavbarBrand>
+        <NavbarText className = "navtext">Social Perception Lab</NavbarText>
+        <NavbarToggler onClick = {toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">People</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/"> Publications</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/"> Location</NavLink>
+            </NavItem>
+          </Nav>
+          
+        </Collapse>
+
+
+      </Navbar>
+    </div>
+  );
+}
+
+export default HeaderComponent;
